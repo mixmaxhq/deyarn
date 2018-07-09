afec2fb65b5f73ff9b5259edcbd00adc7cd92d91
Adam Conrad <422184+acconrad@users.noreply.github.com>
2018-07-09 13:04:35 +0000

    remove the engines.yarn path from the package json
    A copy of [this PR](https://github.com/mixmaxhq/deyarn/pull/8) but simplified based on what the creator of `edit-json-file` wants to do

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ module.exports = () => {
   const packageJson = editJsonFile(packageJsonPath, {
     autosave: true
   });
-  packageJson.set('engines.yarn', 'YARN NO LONGER USED - use npm instead.');
+  packageJson.set('engines.yarn', undefined);
 
   // Output success.
 
